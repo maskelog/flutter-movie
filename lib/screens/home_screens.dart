999ce1b788aaed6bf9b13da65673fc9387535528
maskelog <blitzmaske@gmail.com>
2024-02-16 23:31:48 +0900

    popular poster 추가

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -22,22 +22,68 @@ class HomeScreen extends StatelessWidget {
         future: movies,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                var movie = snapshot.data![index];
-                return Text(movie.originalTitle);
-              },
-              separatorBuilder: (context, index) => const SizedBox(
-                width: 20,
-              ),
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 200,
+                ),
+                Expanded(
+                  child: makeList(snapshot),
+                )
+              ],
             );
           }
           return const Center(
             child: CircularProgressIndicator(),
           );
         },
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<MoviesModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      itemBuilder: (context, index) {
+        var movie = snapshot.data![index];
+        String imageUrl = 'https://image.tmdb.org/t/p/w500${movie.posterPath}';
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 15,
+                        offset: const Offset(10, 15),
+                        color: Colors.black.withOpacity(0.5))
+                  ]),
+              child: Image.network(
+                imageUrl,
+                headers: const {
+                  'Referer': 'https://image.tmdb.org/t/p/w500',
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              movie.originalTitle,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            )
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
       ),
     );
   }
